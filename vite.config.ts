56214e5a9495b666369e9bed00ed6a4314af263d
n1llls <mks.morzee.06@gmail.com>
2025-08-10 03:07:21 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // ⚠️ IMPORTANT: set 'base' to your repo name when deploying to GitHub Pages, e.g. '/dropshipping-crm/'
 export default defineConfig({
   plugins: [react()],
-  base: '/REPLACE_WITH_REPO_NAME/'
+  base: '/drop-crm/'
 })
